d95a09f21a44aee1c1c93849ee726354e8782452
Jason Hancock <jhancock@llnw.com>
2014-09-18 16:05:35 -0700

    adding map

--- www/index.php
+++ www/index.php
@@ -7,13 +7,16 @@ include_once('header.php');
 <div style="text-align: center">
     <img width="400px" height="400px" src="/assets/images/logo.png" alt="Horse Thief Brewers Association" /> <br />
 
-    <p style="margin-top: 25px; margin-bottom: 25px">We meet every 3rd Thursday of the month at 7PM at Clayton Brewing Company in San Dimas, CA.</p>
-
-    <div id="social">
+    <div id="social" style="margin: 25px">
         <a target="_blank" href="https://www.facebook.com/pages/Horse-Thief-Brewers-Association/156373287885298" title="Facebook"><i class="fa fa-facebook fa-2x"></i></a>
         <a target="_blank" href="https://twitter.com/htbrewers" title="Twitter"><i class="fa fa-twitter fa-2x"></i></a>
         <a href="mailto:horsethiefba@gmail.com" title="Email"><i class="fa fa-envelope fa-2x"></i></a>
     </div>
+
+    <p style="margin: 25px">We meet every 3rd Thursday of the month at 7PM at Clayton Brewing Company in San Dimas, CA.</p>
+
+    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3303.5736513328475!2d-117.81778999999999!3d34.10606100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c32f3a1b85a841%3A0xb668cf2f05321cc6!2sClayton+Brewing+Co!5e0!3m2!1sen!2sus!4v1411081019459" width="400" height="300" frameborder="0" style="border:0"></iframe>
+
 </div>
 
 <?php
