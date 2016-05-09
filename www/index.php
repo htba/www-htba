d60840aca5969617ddfae1ac8648f75a1ee999fe
Jason Hancock <jason.b.hancock@jpl.nasa.gov>
2016-05-09 13:27:58 -0700

    schf updates

--- www/index.php
+++ www/index.php
@@ -4,12 +4,6 @@ include_once('header.php');
 
 ?>
 
-<div class="alert alert-info" role="alert" style="text-align: center">
-    <strong>Southern California Homebrew Festival Info</strong> <br />
-
-    The club will be attending the SCHF April 29-30, at Lake Vail. Click <a href="schf.php">here</a> for club-specific information. For general SCFH info, see <a href="http://www.calhomebrewers.org/">here</a>.
-</div>
-
 <div style="text-align: center">
     <img width="400px" height="400px" src="/assets/images/logo.png" alt="Horse Thief Brewers Association" /> <br />
 
@@ -25,7 +19,23 @@ include_once('header.php');
 
     <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3303.5736513328475!2d-117.81778999999999!3d34.10606100000001!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80c32f3a1b85a841%3A0xb668cf2f05321cc6!2sClayton+Brewing+Co!5e0!3m2!1sen!2sus!4v1411081019459" width="400" height="300" frameborder="0" style="border:0"></iframe>
 
+    <br />
+    <br />
+    <br />
+    <br />
+    <br />
+
+    <img src="assets/images/schf_2016.jpg" width="400" />
+
+    <br />
+
+    <div class="caption">
+        <p>The club at the 2016 Southern California Homebrew Festival</p>
+    </div>
+
+
 </div>
+
 
 <?php
 include_once('footer.php');
